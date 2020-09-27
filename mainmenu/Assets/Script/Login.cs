0b9d07ab3ed2bd1c7fd7ad3974c06fbcc596e4d1
Nur Amiera <miera.fyp@gmail.com>
2020-09-27 17:27:41 +0800

    Login
    - User login success

---
+++ mainmenu/Assets/Script/Login.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public InputField nameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    //middle man for class for calling enumarator 
+    public void CallLogin()
+    {
+        StartCoroutine(LoginPlayer());
+    }
+
+    IEnumerator LoginPlayer()
+    {
+        //passing information 
+        //form = pass information to server, more secured 
+        WWWForm form = new WWWForm();
+        //creating empty form 
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        
+        WWW www = new WWW("http://localhost/sqlconnect/login.php", form);
+        yield return www;
+        
+
+        // if we have log in 
+        if (www.text[0] == '0')
+        {
+            DBManager.username = nameField.text;
+            //to ensure that it pass the 2nd information which is score 
+            DBManager.score = int.Parse(www.text.Split('\t')[1]);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            
+        }
+        else
+        {
+            Debug.Log("User Logging Failed. Error # " + www.text);
+        }
+    }
+
+    public void VerifyInput()
+    {
+        submitButton.interactable = (nameField.text.Length >= 4 && passwordField.text.Length >= 4);
+    }
+}
--- mainmenu/Assets/Script/MainMenu.cs
+++ mainmenu/Assets/Script/MainMenu.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Text playerDisplay;
+
+    private void Start()
+    {
+        if (DBManager.LoggedIn)
+        {
+            playerDisplay.text = "Welcome " + DBManager.username;
+        }
+    }
+
     public void GoToRegister()
     {
         SceneManager.LoadScene(1);
         //load scene registermenu
+                          
+    }
 
+    public void GoToLogin()
+    {
+        SceneManager.LoadScene(2);
+        //load scene loginmenu
 
-       
     }
 }
