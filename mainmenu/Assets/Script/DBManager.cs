8529f3d903e919090ac46221a9a33400f2ea17e3
Nur Amiera <miera.fyp@gmail.com>
2020-10-18 13:06:18 +0800

    Adding Interactable script on assesement scene
    - adding script interactable
    - adding script interantable
    -

--- mainmenu/Assets/Script/DBManager.cs
+++ mainmenu/Assets/Script/DBManager.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public static class DBManager 
 {
     public static string username;
     public static int score;
+    public static int duration;
+    public static int dbtest;
+    public static decimal dbfloat;
 
     //use to check are we logged in 
     public static bool LoggedIn
